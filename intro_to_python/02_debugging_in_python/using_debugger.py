2a71b35e03eb9c382be7436ae59e1bca58272129
Shelby Talbert <shelbytalbert@Shelbys-MBP.fios-router.home>
2022-03-08 18:42:07 -0500

    oooh understanding the tests a bit better. great refresher on TDD

--- intro_to_python/02_debugging_in_python/using_debugger.py
+++ intro_to_python/02_debugging_in_python/using_debugger.py
@@ -7,22 +7,26 @@ def greeting(personalize='World'):
     return 'Hello ' + personalize
 
 
-def shopping_list(items):
+def shopping_list(items, oxfordRule=True):
     result = 'I need to get: '
     arrLength = len(items)
     for i in range(len(items)):
         if i == arrLength - 1:
             result += items[i]
         elif i == arrLength - 2:
-            result += items[i] + ' and '
+            if oxfordRule == False:
+                result += items[i] + ' and '
+            else:
+                result += items[i] + ', and '  
         else:
-            result += items[i] + ', '      
+            result += items[i] + ', '        
+    # import pdb; pdb.set_trace()   
     return result
 
 
 
-def some_function():
-    import pdb; pdb.set_trace()
+# def some_function():
+#     import pdb; pdb.set_trace()
 
 
-some_function()
+# some_function()
