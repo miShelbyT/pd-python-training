a26165d78333309992ffa2443c1c8c09465017da
Shelby Talbert <shelbytalbert@Shelbys-MBP.fios-router.home>
2022-03-09 11:55:30 -0500

    lolol finally. passed the tests. such a good refresher on reading tests so i can better help my students

--- intro_to_python/02_debugging_in_python/using_debugger.py
+++ intro_to_python/02_debugging_in_python/using_debugger.py
@@ -7,14 +7,14 @@ def greeting(personalize='World'):
     return 'Hello ' + personalize
 
 
-def shopping_list(items, oxfordRule=True):
+def shopping_list(items, oxfordComma=True):
     result = 'I need to get: '
     arrLength = len(items)
     for i in range(len(items)):
         if i == arrLength - 1:
             result += items[i]
         elif i == arrLength - 2:
-            if oxfordRule == False:
+            if oxfordComma == False or arrLength == 2:
                 result += items[i] + ' and '
             else:
                 result += items[i] + ', and '  
@@ -23,7 +23,7 @@ def shopping_list(items, oxfordRule=True):
     # import pdb; pdb.set_trace()   
     return result
 
-
+# shopping_list(['chips', 'fruit'])
 
 # def some_function():
 #     import pdb; pdb.set_trace()
